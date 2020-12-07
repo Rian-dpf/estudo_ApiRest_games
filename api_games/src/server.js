ac239030a356681015bf604474a3dac4fc5955a0
Rian dos Passos <riandpf2002@gmail.com>
2020-12-07 18:11:51 -0300

    Add Authentication Api

--- api_games/src/server.js
+++ api_games/src/server.js
@@ -1,9 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const connection = require("./database/db");
-const Game = require("./database/games");
+const Game = require("./database/game");
+const User = require("./database/user");
 const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const app = express();
+
+const JWTSecret = "uhdfuiufn$oe&i475*";
+
+function auth(req, res, next) {
+    const authToken = req.headers["authorization"];
+
+    if (authToken != undefined) {
+        const bearer = authToken.split(" ");
+        const token = bearer[1];
+
+        jwt.verify(token, JWTSecret, (err, data) => {
+            if (err) {
+                res.status(401);
+                res.json({ err: "Não autorizado!" });
+            } else {
+                req.token = token;
+                req.loggedUser = { id: data.id, email: data.email };
+                next();
+            }
+        });
+        console.log(bearer);
+    } else {
+        res.status(401);
+        res.json({ err: "Token inválido!" });
+    }
+}
 
 app.use(cors());
 
@@ -21,7 +51,7 @@ connection
         console.log(err);
     });
 
-app.get("/games", async(req, res) => {
+app.get("/games", auth, async(req, res) => {
     try {
         const games = await Game.findAll();
         games ? res.status(200).json(games) : res.sendStatus(204);
@@ -31,7 +61,7 @@ app.get("/games", async(req, res) => {
     }
 });
 
-app.get("/game/:id", async(req, res) => {
+app.get("/game/:id", auth, async(req, res) => {
     let id = req.params.id;
     if (isNaN(id)) {
         res.sendStatus(400);
@@ -50,7 +80,7 @@ app.get("/game/:id", async(req, res) => {
     }
 });
 
-app.post("/game", (req, res) => {
+app.post("/game", auth, (req, res) => {
     const { title, price, year } = req.body;
 
     if (title != undefined && price != undefined && year != undefined) {
@@ -70,7 +100,7 @@ app.post("/game", (req, res) => {
     }
 });
 
-app.delete("/game/:id", (req, res) => {
+app.delete("/game/:id", auth, (req, res) => {
     let id = req.params.id;
 
     if (isNaN(id)) {
@@ -90,7 +120,7 @@ app.delete("/game/:id", (req, res) => {
     }
 });
 
-app.put("/game/:id", (req, res) => {
+app.put("/game/:id", auth, (req, res) => {
     let id = req.params.id;
 
     let { title, price, year } = req.body;
@@ -123,6 +153,86 @@ app.put("/game/:id", (req, res) => {
                 res.sendStatus(400);
             }
         });
+    }
+});
+
+app.post("/user", (req, res) => {
+    const { email, password } = req.body;
+
+    if (email != undefined) {
+        if (password != undefined) {
+            User.findOne({
+                where: {
+                    email: email,
+                },
+            }).then((user) => {
+                if (user == undefined) {
+                    const salt = bcrypt.genSaltSync(10);
+                    const hash = bcrypt.hashSync(password, salt);
+
+                    User.create({
+                            email: email,
+                            password: hash,
+                        })
+                        .then(() => {
+                            res.sendStatus(200);
+                        })
+                        .catch((e) => {
+                            console.log(e);
+                        });
+                } else {
+                    res.sendStatus(422);
+                }
+            });
+        } else {
+            res.status(400);
+            res.json({ err: "Digite uma senha" });
+        }
+    } else {
+        res.status(400);
+        res.json({ err: "Digite um email" });
+    }
+});
+
+app.post("/auth", (req, res) => {
+    const { email, password } = req.body;
+
+    if (email != undefined) {
+        User.findOne({
+            where: {
+                email: email,
+            },
+        }).then((user) => {
+            if (user != undefined) {
+                const correct = bcrypt.compareSync(password, user.password);
+
+                if (correct) {
+                    jwt.sign({ id: user.id, email: user.email },
+                        JWTSecret, {
+                            expiresIn: "48h",
+                        },
+                        (err, token) => {
+                            if (err) {
+                                res.status(400);
+                                res.json({ err: "Falha interna" });
+                            } else {
+                                res.status(200);
+                                res.json({ token: token });
+                            }
+                        }
+                    );
+                } else {
+                    res.status(400);
+                    res.json({ err: "Senha incorreta!" });
+                }
+            } else {
+                res.status(404);
+                res.json({ err: "O usuário não existe na base de dados!" });
+            }
+        });
+    } else {
+        res.status(400);
+        res.json({ err: "Digite o email!" });
     }
 });
 
